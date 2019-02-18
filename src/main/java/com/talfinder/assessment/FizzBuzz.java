fd5eba7e8ba673b90c4a96db2b7c4900a60e42e0
Harsha Bhargav <hbhargav@users.noreply.github.com>
2019-02-18 13:46:03 +0530

    Update FizzBuzz.java

--- src/main/java/com/talfinder/assessment/FizzBuzz.java
+++ src/main/java/com/talfinder/assessment/FizzBuzz.java
@@ -8,6 +8,12 @@ import java.util.List;
  */
 public class FizzBuzz {
   public List<String> evaluateFizzBuzz(int min, int max) {
+    /*
+     * NOTE:
+     * 1. Write your solution within this method
+     * 2. Do not make changes to the Class Name, Method Name or Method Signature
+     * 3. Use the BuildAndRunTests command to evaluate your solution
+     */
     List<String> fizzBuzz = new ArrayList<>();
     return fizzBuzz;
   }
