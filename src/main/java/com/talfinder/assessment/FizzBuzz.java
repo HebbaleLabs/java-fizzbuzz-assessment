ee38999fcfe45e0e1182381891585b9171e8d218
Harsha Bhargav <hbhargav@users.noreply.github.com>
2019-02-21 17:40:25 +0530

    Update FizzBuzz.java

--- src/main/java/com/talfinder/assessment/FizzBuzz.java
+++ src/main/java/com/talfinder/assessment/FizzBuzz.java
@@ -12,7 +12,8 @@ public class FizzBuzz {
      * NOTE:
      * 1. Write your solution within this method
      * 2. Do not make changes to the Class Name, Method Name or Method Signature
-     * 3. Use the BuildAndRunTests command to evaluate your solution
+     * 3. Use the Run-Java command to run main method
+     * 4. Use the BuildAndRunTests command to evaluate your solution
      */
     List<String> fizzBuzz = new ArrayList<>();
     return fizzBuzz;
